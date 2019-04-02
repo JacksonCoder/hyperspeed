8b22814a4c1b778081e70ec683fe67e60fa5bb91
Jackson Lewis <st.japa6@gmail.com>
2019-04-02 21:53:22 +0000

    Rewrite comments in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 
-// Star Engine is a fusion of an ECS and an observer-event system.
+// Hyperspeed Engine is a fusion of an ECS and an observer-event system.
 // For example, how would picking up an item work with an ECS?
+// With Hyperspeed, it's easy: an Observer checks for a player clicking on an item and registers that
+// they should pick up the item: this is then handled by either a controller or a system accessing a system resource.
 
 extern crate specs;
 
