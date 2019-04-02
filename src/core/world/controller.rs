439d57ced065db9f70d9478a10ee92aa9da5aeff
Jackson Lewis <st.japa6@gmail.com>
2019-04-02 21:52:54 +0000

    Add Controller documentation

--- src/core/world/controller.rs
+++ src/core/world/controller.rs
@@ -1,5 +1,9 @@
 use super::World;
 
+/// Controllers are a unique feature of Hyperspeed. Controllers can be seen
+/// as 'meta-systems'. They are for scenarios like global subsystems that need to
+/// modify the entire entity system and also respond to events that are emitted
+/// by an Observer.
 pub trait Controller {
     type ObserverEvent;
     fn tick(&mut self, _world: &mut World, _delta_time: f32) {}
