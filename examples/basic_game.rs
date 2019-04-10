bae10ef44b9bc2e74c2224e0778555c22aca56e1
Jackson Lewis <st.japa6@gmail.com>
2019-04-09 17:07:54 -0700

    Update basic_game

--- examples/basic_game.rs
+++ examples/basic_game.rs
@@ -41,6 +41,7 @@ struct MotionSystem;
 
 struct PickupController;
 
+#[derive(Clone)]
 enum Event {
     Empty,
     ItemPickup {
@@ -52,27 +53,6 @@ enum Event {
 impl Default for Event {
     fn default() -> Event {
        Event::Empty
-    }
-}
-
-impl Controller for PickupController {
-    type ObserverEvent = Event;
-    fn event(&mut self, world: &mut World, e: &Self::ObserverEvent) {
-        match e {
-            Event::ItemPickup { item_id, player_id } => {
-                let mut write_item = world.ecs_world.write_storage::<Item>();
-                let mut write_player = world.ecs_world.write_storage::<Player>();
-                let mut maybe_item = write_item.get_mut(*item_id);
-                let mut maybe_player = write_player.get_mut(*player_id);
-                if let Some(item) = maybe_item {
-                    if let Some(player) = maybe_player {
-                        item.inside = Some(player_id.clone());
-                        player.items.push(item_id.clone());
-                    }
-                }
-            }
-            _ => {}
-        }
     }
 }
 
@@ -88,12 +68,6 @@ impl<'a> System<'a> for MoveSystem {
     }
 }
 
-struct O;
-
-impl Observer for O {
-    type ObserverEvent = Event;
-}
-
 struct M;
 
 impl MasterController for M {
@@ -101,17 +75,18 @@ impl MasterController for M {
     
     fn tick(&mut self, world: &mut World, delta_time: f32) -> EngineInstruction {
         EngineInstruction::Run {
-            run_controllers: true,
-            run_observer: true,
-            run_systems: true
+            run_dispatcher: true
         }
     }
 }
 
 fn main() {
-    let mut engine = Engine::new().with_observer(O {})
-                                  .with_mc(M {})
+    let mut engine = Engine::new().with_mc(M {})
                                   .build().unwrap();
     
-    engine.tick();
+    engine.start_server();
+
+    loop {
+        engine.tick();
+    }
 }
