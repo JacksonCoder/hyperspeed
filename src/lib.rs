17e856e8857ea5ab65b053c51944cc7d1169f6b3
Jackson Lewis <st.japa6@gmail.com>
2019-04-16 10:04:04 -0700

    Remove outdated information

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![allow(dead_code)]
 #![feature(duration_float)]
 
-// Hyperspeed Engine is a fusion of an ECS and an observer-event system.
-// For example, how would picking up an item work with an ECS?
-// With Hyperspeed, it's easy: an Observer checks for a player clicking on an item and registers that
-// they should pick up the item: this is then handled by either a controller or a system accessing the Events resource.
 
 extern crate specs;
 
