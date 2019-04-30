a3bf7dd5f048daec04fc7901594133f7c5ad8b97
Jackson Lewis <st.japa6@gmail.com>
2019-04-30 10:34:11 -0700

    Add additional features to connectioncollection and privatize fields

--- src/core/world/connection.rs
+++ src/core/world/connection.rs
@@ -1,12 +1,15 @@
-#[derive(Clone, Debug)]
+use std::collections::VecDeque;
+
+#[derive(Clone, Debug, Default)]
 pub struct ConnectionCollection {
-    pub connections: Vec<Connection>
+    new_keys: VecDeque<String>,
+    connections: Vec<Connection>
 }
 
 // Sharing connections can be hard, because both the ECS system and the server
 // need to read/write to the client views. In this case, the world's connection collection
 // is passed immutably to the server, which sends data to each respective channel.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Connection {
     pub key: String
 }
@@ -20,11 +23,39 @@ pub struct ClientView {
 impl ConnectionCollection {
     pub fn new() -> Self {
         ConnectionCollection {
+            new_keys: VecDeque::new(),
             connections: vec![]
         }
     }
 
     pub fn size(&self) -> usize {
         self.connections.len()
+    }
+
+    pub fn pop_new_key(&mut self) -> Option<String> {
+        self.new_keys.pop_front()
+    }
+
+    pub fn pop_new_keys(&mut self) -> Vec<String> {
+        let mut keys = vec!();
+        loop {
+            let key = self.pop_new_key();
+            if key.is_some() {
+                keys.push(key.unwrap());
+            } else {
+                break;
+            }
+        }
+        self.new_keys.clear();
+        keys
+    }
+
+    pub fn remove(&mut self, key: &String) {
+        self.connections.retain(|x| x.key == *key);
+    }
+
+    pub fn push(&mut self, c: Connection) {
+        self.new_keys.push_back(c.key.clone());
+        self.connections.push(c);
     }
 }
